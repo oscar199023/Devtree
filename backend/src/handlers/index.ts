ede00ce8ac6ce1751fdd5e9fa47b1494cab57b21
Oscar Ayala <fabianayala675@gmail.com>
2025-08-21 13:33:53 -0400

    Generando el JWT

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -59,7 +59,7 @@ export const login = async (req: Request, res: Response) => {
         return res.status(401).json({ error: error.message })
     }
 
-    generateJWT(user)
+    const token = generateJWT({id: user._id})
 
-    res.send('Login exitoso')
+    res.send(token)
 }
--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,4 +1,9 @@
+import  jwt,  {JwtPayload} from "jsonwebtoken"
 
-export const generateJWT = (payload) => {
-    console.log(payload)
+
+export const generateJWT = (payload : JwtPayload) => {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+    return token
 }
