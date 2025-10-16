a6886e5995d58426c0372f807e00a8c977fdccdf
Oscar Ayala <fabianayala675@gmail.com>
2025-10-15 22:30:58 -0300

    identificando el Usuario auntenticado por el JWT

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { validationResult } from 'express-validator'
 import slug from 'slug'
+import jwt from 'jsonwebtoken'
 import User from "../models/User"
 import { checkPassword, hashPassword } from '../utils/auth'
 import { generateJWT } from '../utils/jwt'
@@ -64,5 +65,26 @@ export const getUser = async (req: Request, res: Response) => {
     if (!bearer){
         const error = new Error('No autorizado')
         return res.status(401).json({ error: error.message })
+    }
+
+    const [, token] = bearer.split(' ')
+
+    if (!token) {
+        const error = new Error('No autorizado')
+        return res.status(401).json({ error: error.message })
+    }
+
+    try {
+        const result = jwt.verify(token, process.env.JWT_SECRET)
+        if (typeof result === 'object' && result.id) {
+            const user = await await User.findById(result.id).select('-password')
+            if (!user) {
+                const error = new Error('El usuario no existe')
+                return res.status(404).json({ error: error.message })
+            }
+            res.json({ user })
+        }
+    } catch (error) {
+        res.status(401).json({ error: 'Token no valido' })
     }
 }
