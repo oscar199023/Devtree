157bc35c5cc88288cf0deb3ca739e1e2cf8bd8e2
Oscar Ayala <fabianayala675@gmail.com>
2025-10-22 23:17:32 -0300

    Leyendo el archivo que se sube en React

--- frontend/src/view/ProfileView.tsx
+++ frontend/src/view/ProfileView.tsx
@@ -28,6 +28,13 @@ export default function ProfileView() {
         }
     })
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            console.log(e.target.files[0])
+        }
+
+    }
+
     const handleUserProfileForm = (formData: ProfileForm) => {
         updateProfileMutation.mutate(formData)
     }
@@ -77,7 +84,7 @@ export default function ProfileView() {
                     name="handle"
                     className="border-none bg-slate-100 rounded-lg p-2"
                     accept="image/*"
-                    onChange={ () => {} }
+                    onChange={ handleChange }
                 />
             </div>
 
