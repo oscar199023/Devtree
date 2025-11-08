3ef361a9799670a7abc2bb9f6dd9f603873ad647
Oscar Ayala <fabianayala675@gmail.com>
2025-11-07 21:26:17 -0300

    Agregando el Array de Redes Sociales

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -13,9 +13,16 @@ export type RegisterForm = Pick<User, 'handle' | 'name' | 'email'> & {
     password_confirmation: string
 
 }
-
 export type LoginForm = Pick<User, 'email'> & {
     password: string
 }
-
 export type ProfileForm = Pick<User, 'handle' | 'description'>
+
+export type SocialNetwork = {
+    id: number
+    name: string
+    url: string
+    enabled: boolean
+}
+export type DevTreeLink = Pick<SocialNetwork, 'name' | 'url' | 'enabled'>
+
--- frontend/src/view/LinkTreeView.tsx
+++ frontend/src/view/LinkTreeView.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react'
+import { social } from '../data/social'
 
 export default function LinkTreeView() {
+  const [devTreeLinks, setDevTreeLinks] = useState(social)
+
   return (
     <div>LinkTreeView</div>
   )
