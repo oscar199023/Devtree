6372c49bed56cea44cccca13c1c0abc0182b1523
Oscar Ayala <fabianayala675@gmail.com>
2025-11-08 01:09:11 -0300

    Detectando si una URL es valida antes de habilitarla

--- frontend/src/view/LinkTreeView.tsx
+++ frontend/src/view/LinkTreeView.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react'
 import { social } from '../data/social'
 import DevTreeInput from '../components/DevTreeInput'
+import { isValidUrl } from '../utils'
+import { toast } from 'sonner'
 
 export default function LinkTreeView() {
   const [devTreeLinks, setDevTreeLinks] = useState(social)
 
   const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? {...link, url: e.target.value }: link)
-    console.log(updatedLinks)
     setDevTreeLinks(updatedLinks)
   }
 
   const handleEnableLink = (socialNetwork: string) => {
-    const updatedLinks = devTreeLinks.map(link => link.name === socialNetwork ? {...link, enabled: !link.enabled }: link)
+    const updatedLinks = devTreeLinks.map(link => {
+      if (link.name === socialNetwork) {
+        if (isValidUrl(link.url)) {
+          return {...link, enabled: !link.enabled }
+        } else {
+          toast.error('URL no valida')
+        }
+      }
+      return link
+    })
     console.log(updatedLinks)
     setDevTreeLinks(updatedLinks)
 
