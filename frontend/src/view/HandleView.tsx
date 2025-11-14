2273292db5a8b6770308954d0e4c3f5ac0b6b794
Oscar Ayala <fabianayala675@gmail.com>
2025-11-13 21:49:33 -0300

    Consultando el Tree con React Query

--- frontend/src/view/HandleView.tsx
+++ frontend/src/view/HandleView.tsx
@@ -1,6 +1,15 @@
+import { useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
+import { getUserByHandle } from "../api/DevTreeAPI"
 
 export default function HandleView() {
-  return (
-    <div>HandleView</div>
-  )
+    const params = useParams()
+    const handle = params.handle!
+    const { data, error, isLoading} = useQuery({
+        queryFn: () => getUserByHandle(handle),
+        queryKey: ['handle', handle]
+    })
+    return (
+        <div>HandleView</div>
+    )
 }
