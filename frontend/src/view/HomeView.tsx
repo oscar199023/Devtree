63754732db47233cafbe9b55e6b02bcb770553ee
Oscar Ayala <fabianayala675@gmail.com>
2025-11-15 15:41:18 -0300

    Interactuando con la pagina Principal

--- frontend/src/view/HomeView.tsx
+++ frontend/src/view/HomeView.tsx
@@ -3,7 +3,20 @@ import Header from "../components/Header";
 export default function HomeView() {
   return (
     <> 
-        <Header />
+      <Header />
+      <main className="bg-gray-100 py-10 min-h-screen">
+        <div className="max-w-5x1 mx-auto mt-10">
+          <div className="lg:w-1/2 px-10 lg:p-0 space-y-6">
+            <h1 className="text-6xl font-black">
+              Todas tus <span className="text-cyan-400">Redes Sociales </span>
+              en un enlace
+            </h1>
+            <p className="text-slate-800 text-xl">Unete a más de 200 mil developers compartiendo
+              sus redes sociales, comparte tu perfil de Tiktok, Facebook, Instagram, Youtube, Github y más
+            </p>
+          </div>
+        </div>
+      </main> 
     
     </>
   )
