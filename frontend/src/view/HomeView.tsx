126b0cef0714fa77987f9c98d6e98760e8e9f0c5
Oscar Ayala <fabianayala675@gmail.com>
2025-11-15 15:53:30 -0300

    Añadiendo la imagen de fondo en el inicio

--- frontend/src/view/HomeView.tsx
+++ frontend/src/view/HomeView.tsx
@@ -4,7 +4,8 @@ export default function HomeView() {
   return (
     <> 
       <Header />
-      <main className="bg-gray-100 py-10 min-h-screen">
+
+      <main className="bg-gray-100 py-10 min-h-screen bg-no-repeat bg-right lg:bg-home lg:bg-home-xl">
         <div className="max-w-5x1 mx-auto mt-10">
           <div className="lg:w-1/2 px-10 lg:p-0 space-y-6">
             <h1 className="text-6xl font-black">
